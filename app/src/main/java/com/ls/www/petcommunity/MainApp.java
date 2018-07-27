10705aac66b84059c23ceb280566c412eaf93955
shi.rei <shi.rei@vega-c.com>
2018-07-27 16:40:53 +0900

    add splash activity and new ui

---
+++ app/src/main/java/com/ls/www/petcommunity/MainApp.java
@@ -0,0 +1,43 @@
+package com.ls.www.petcommunity;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
+import java.io.File;
+
+public class MainApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public static void initImgLoader(Context context) {
+        File cacheDir = StorageUtils.getOwnCacheDirectory(context,"imageloader/Cache");
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(context)
+                .memoryCacheExtraOptions(480, 800)
+                .threadPoolSize(3)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator()) //将保存的时候的URI名称用MD5 加密
+                .memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024)) // You can pass your own memory cache implementation/你可以通过自己的内存缓存实现
+                .memoryCacheSize(2 * 1024 * 1024) // 内存缓存的最大值
+                .diskCacheSize(50 * 1024 * 1024)  // 50 Mb sd卡(本地)缓存的最大值
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                // 由原先的discCache -> diskCache
+                .diskCache(new UnlimitedDiskCache(cacheDir))//自定义缓存路径
+                .imageDownloader(new BaseImageDownloader(context, 5 * 1000, 30 * 1000)) // connectTimeout (5 s), readTimeout (30 s)超时时间
+                .writeDebugLogs() // Remove for release app
+                .build();
+        ImageLoader.getInstance().init(configuration);
+    }
+}
---
+++ app/src/main/java/com/ls/www/petcommunity/activity/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.ls.www.petcommunity.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import com.ls.www.petcommunity.MainActivity;
+import com.ls.www.petcommunity.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cn.bmob.v3.Bmob;
+
+public class SplashActivity extends AppCompatActivity {
+    private Intent intent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        // 默认初始化Bmob
+        Bmob.initialize(this, "c1c676e641526dc255e7dc4904eb6c86");
+
+        intent = new Intent(this, MainActivity.class);
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(intent); //执行意图
+                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+            }
+        };
+        timer.schedule(task, 1000 * 2); //2秒后跳转
+
+    }
+}
